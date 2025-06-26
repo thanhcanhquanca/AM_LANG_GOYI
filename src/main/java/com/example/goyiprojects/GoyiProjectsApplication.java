3c0f77768a131757f239d6ae7c24b4ca80fbe203
thanhcanhquanca <chuanhquan89@gmail.com>
2025-06-26 11:57:11 +0700

    init

---
+++ src/main/java/com/example/goyiprojects/GoyiProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.example.goyiprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoyiProjectsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoyiProjectsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goyiprojects/GoyiProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goyiprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoyiProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
